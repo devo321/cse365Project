89534d03d57cfd76d41eea956d2714ac38c8247d
schultz <nashrschultz@gmail.com>
2020-11-28 20:42:22 -0700

    Fixed spelling mistake

--- Project/FinalProject/src/World.java
+++ Project/FinalProject/src/World.java
@@ -35,7 +35,7 @@ public class World extends JFrame implements ActionListener {
 		aboutMenu = new JMenuItem("About");
 		
 		loadRoster = new JMenuItem("Load Roster");
-		addAttendance = new JMenuItem("Add Attendace");
+		addAttendance = new JMenuItem("Add Attendance");
 		save = new JMenuItem("Save");
 		plotData = new JMenuItem("Plot Data");
 		
